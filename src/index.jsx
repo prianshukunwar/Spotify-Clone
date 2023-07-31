144342e4192c5d38492b47c69a3680027012eb33
Priyanshu Kunwar <priyanshukunwar7@gmail.com>
2023-07-31 22:12:20 +0530

    add: files in vite config, index, deploy, package

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -10,9 +10,9 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Spotify-Clone">
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>,
 );
